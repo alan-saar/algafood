683d701f6ad947860e47283969d7c994cdb6c943
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-25 19:49:59 -0300

    DDL em modo drop create com import.sql e primeiros modelos

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Cozinha
+ */
+@Entity
+@Table(name = "cozinha")
+@EqualsAndHashCode(of = {"id"})
+public @Data class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 50)
+    private String nome;
+
+
+}
---
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Restaurante
+ */
+@Entity
+@EqualsAndHashCode(of = {"id"})
+public @Data class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+}
