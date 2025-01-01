9ee66078ae1fa745527fa1d54d63be672d12ba06
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-01-01 15:10:03 -0300

    3.11 atualizando obj

--- src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/ConsultaCozinhaMain.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.jpa;
 
-import java.util.List;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -22,22 +21,31 @@ public class ConsultaCozinhaMain {
         // pego o bean direto da aplicação
         CadastroCozinha cadastroCozinha = appContext.getBean(CadastroCozinha.class);
 
+        // lista
         // List<Cozinha> cozinhas = cadastroCozinha.listar();
         // System.out.println("inicio");
         // cozinhas.forEach(c -> System.out.println(c.getNome()));
         // System.out.println("fim");
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Brasileira");
+        // adiciona
+        // Cozinha cozinha1 = new Cozinha();
+        // cozinha1.setNome("Brasileira");
+        //
+        // Cozinha cozinha2 = new Cozinha();
+        // cozinha2.setNome("Japonesa");
+        //
+        // cozinha1 = cadastroCozinha.adicionar(cozinha1);
+        // cozinha2 = cadastroCozinha.adicionar(cozinha2);
 
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Japonesa");
+        // busca pelo id
+        // Cozinha cozinha3 = cadastroCozinha.buscar(3L);
+        // System.out.println("cozinha3: " + cozinha3.getNome());
 
-        cozinha1 = cadastroCozinha.adicionar(cozinha1);
-        cozinha2 = cadastroCozinha.adicionar(cozinha2);
-
-        Cozinha cozinha3 = cadastroCozinha.buscar(3L);
-        System.out.println("cozinha3: " + cozinha3.getNome());
+        // atualiza
+        Cozinha cozinha4 = new Cozinha();
+        cozinha4.setId(1L);
+        cozinha4.setNome("Brasileira");
+        cadastroCozinha.salvar(cozinha4);
 
     }
 
