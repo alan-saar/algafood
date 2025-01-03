e3ba3b57fb7c85b1a1c64f973bb48a618b41a58f
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-01-03 15:24:20 -0300

    3.17 @ManyToOne

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -25,5 +26,8 @@ public @Data class Restaurante {
 
     @Column(name = "taxa_frete")
     private BigDecimal taxaFrete;
+
+    @ManyToOne
+    private Cozinha cozinha;
 
 }
--- src/test/java/com/algaworks/algafood/infrastructures/repository/RestauranteRepositoryJPATest.java
+++ src/test/java/com/algaworks/algafood/infrastructures/repository/RestauranteRepositoryJPATest.java
@@ -4,7 +4,9 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import static org.assertj.core.api.Assertions.*;
 
@@ -13,6 +15,9 @@ public class RestauranteRepositoryJPATest {
 
     @Autowired
     RestauranteRepository restauranteRepository;
+
+    @Autowired
+    CozinhaRepository cozinhaRepository;
 
     @Test
     void deveRetornarUmaListaDeRestaurantesNaoNula() {
@@ -28,6 +33,20 @@ public class RestauranteRepositoryJPATest {
 
         List<Restaurante> restaurantes = restauranteRepository.listar();
         assertThat(restaurantes.size()).isGreaterThan(0);
+    }
+
+    @Test
+    void deveSerPossivelAdicionarUmaCozinha() {
+        Cozinha cozinha = new Cozinha();
+        cozinha.setNome("Tailandesa");
+        cozinha = cozinhaRepository.salvar(cozinha);
+        Restaurante restaurante = new Restaurante();
+        restaurante.setNome("Copo Sujo");
+        restaurante.setCozinha(cozinha);
+        restauranteRepository.salvar(restaurante);
+        assertThat(restaurante.getCozinha().getId()).isNotNull();
+        System.out.printf("%s - %s\n", restaurante.getNome(),
+                restaurante.getCozinha().getNome());
     }
 
 }
