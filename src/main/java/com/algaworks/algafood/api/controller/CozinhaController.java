40ebec0f59b889203d7ebc2b25c92a264185e446
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-01-25 10:02:50 -0300

    4.14 resource cozinha por id

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -29,4 +30,9 @@ public class CozinhaController {
     // public List<Cozinha> listarxml() {
     // return cozinhaRepository.listar();
     // }
+
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
+        return cozinhaRepository.buscar(id);
+    }
 }
