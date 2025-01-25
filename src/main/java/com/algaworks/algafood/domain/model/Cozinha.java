27077131a92b9a671ec8f163a343812e7c2fcf84
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-01-25 10:25:55 -0300

    4.15 customizando a representação

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonRootName;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,6 +16,7 @@ import lombok.EqualsAndHashCode;
 @Entity
 @Table(name = "cozinha")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@JsonRootName("cozinha") // no jml o nome da entidade será cozinha (e não o nome da classe que tem C maiusculo)
 public @Data class Cozinha {
 
     @Id
@@ -22,6 +24,8 @@ public @Data class Cozinha {
     @EqualsAndHashCode.Include
     private Long id;
 
+    // @JsonIgnore // nao mostra a propriedade na representação
+    // @JsonProperty("titulo") // vai fazer o nome da representação mudar para título
     @Column(name = "nome", length = 50, nullable = false)
     private String nome;
 
