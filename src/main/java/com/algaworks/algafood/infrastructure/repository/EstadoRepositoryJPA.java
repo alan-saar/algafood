8bc56a87e420302d692fe20f8e5c25da5d1f03fb
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-01-25 09:05:20 -0300

    4.11 Collection Resource de estados

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/EstadoRepositoryJPA.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class EstadoRepositoryJPA implements EstadoRepository {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Override
+    public List<Estado> listar() {
+        return manager.createQuery("from Estado", Estado.class).getResultList();
+    }
+
+    @Override
+    public Estado buscar(Long id) {
+        return manager.find(Estado.class, id);
+    }
+
+    @Override
+    @Transactional
+    public Estado salvar(Estado estado) {
+        return manager.merge(estado);
+    }
+
+    @Override
+    public void remover(Estado estado) {
+        estado = buscar(estado.getId());
+        manager.remove(estado);
+    }
+
+
+}
