b03c7b3c9b884916414c1450a58cd5c2b1e098f5
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-01-28 10:31:22 -0300

    2.25 PUT cozinha

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -55,5 +57,20 @@ public class CozinhaController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cozinha adicionar(@RequestBody Cozinha cozinha) {
         return cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping("/{cozinhaId}")
+    public ResponseEntity<Cozinha> atualizar(@PathVariable("cozinhaId") Long cozinhaId, @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaDb = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinhaDb != null) {
+            // cozinhaDb.setNome(cozinha.getNome());
+            BeanUtils.copyProperties(cozinha, cozinhaDb, "id"); // ignora o atributo id. Do terceiro parâmetro para frente são parametros para ignorar
+            cozinhaDb = cozinhaRepository.salvar(cozinhaDb);
+            return ResponseEntity.ok(cozinhaDb);
+        }
+
+        return ResponseEntity.notFound().build();
+
     }
 }
